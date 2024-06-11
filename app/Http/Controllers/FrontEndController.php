b7d7ad9c2551d9df2406159a2e433a313d50e6e5
Naufal Hidayah Surya <naufalhidayah17@gmail.com>
2024-06-11 10:21:22 +0700

    fix : front end condition letter to like

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -13,7 +13,7 @@ class FrontEndController extends Controller
     public function __invoke(Request $request)
     {
         $letter = Letter::where('code', $request->q)
-            ->orWhere('regarding', $request->q)
+            ->orWhere('regarding', 'LIKE', `%$request->q%`)
             ->first();
 
         $title = 'Sukema';
